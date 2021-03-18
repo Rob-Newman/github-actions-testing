0f9b2512d61ae37f94e96f1a2fb95476ceb42b41
Rob Newman <rnewman1000@gmail.com>
2021-03-18 11:13:17 +0000

    Fixed issue with feature x

--- src/github-actions-testing/Program.cs
+++ src/github-actions-testing/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! How are you?");
+            // Pretending to add a bug fix change
         }
     }
 }
